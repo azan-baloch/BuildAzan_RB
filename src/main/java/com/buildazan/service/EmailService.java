e9d1c6805368a9eedf68fa5479ccb38c554da03e
Azan Baloch <balochazan36447@gmail.com>
2025-02-22 19:27:21 +0500

    updated email service

--- src/main/java/com/buildazan/service/EmailService.java
+++ src/main/java/com/buildazan/service/EmailService.java
@@ -11,23 +11,23 @@ import jakarta.mail.internet.MimeMessage;
 @Service
 public class EmailService{
 
-    @Autowired
-    private JavaMailSender mailSender;
+    // @Autowired
+    // private JavaMailSender mailSender;
 
     
     public boolean sendVerificationLinkEmail(String toEmail, String subject, String verificationCode) {
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(toEmail);
-            helper.setSubject(subject);
-            String htmlBody = "<html><body><h2>Verification Link</h2><p>Your verification link is: <a href=\"https://buildazaan.netlify.app/verify-link?link="
-                    + verificationCode
-                    + "\">Click here to verify</a></p><p>Do not share it with anyone.</p><p>Note: Link will expire in 10 minutes</p></body></html>";
-            helper.setText(htmlBody, true);
-            mailSender.send(message);
+            // MimeMessage message = mailSender.createMimeMessage();
+            // MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            // helper.setTo(toEmail);
+            // helper.setSubject(subject);
+            // String htmlBody = "<html><body><h2>Verification Link</h2><p>Your verification link is: <a href=\"https://buildazaan.netlify.app/verify-link?link="
+            //         + verificationCode
+            //         + "\">Click here to verify</a></p><p>Do not share it with anyone.</p><p>Note: Link will expire in 10 minutes</p></body></html>";
+            // helper.setText(htmlBody, true);
+            // mailSender.send(message);
             return true;
-        } catch (MessagingException e) {
+        } catch (Exception e) {
             System.out.println(e);
             return false;
         }
@@ -36,18 +36,18 @@ public class EmailService{
     // Send OTP email
     public boolean sendOTPEmail(String toEmail, String subject, String otp) {
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(toEmail);
-            helper.setSubject(subject);
-            String htmlBody = "<html><body><h2>Your One-Time Password (OTP)</h2>"
-                    + "<p>Your OTP is: <strong>" + otp + "</strong></p>"
-                    + "<p>Please use this code within 10 minutes. Do not share it with anyone.</p>"
-                    + "<p>Note: OTP will expire in 10 minutes.</p></body></html>";
-            helper.setText(htmlBody, true);
-            mailSender.send(message);
+            // MimeMessage message = mailSender.createMimeMessage();
+            // MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            // helper.setTo(toEmail);
+            // helper.setSubject(subject);
+            // String htmlBody = "<html><body><h2>Your One-Time Password (OTP)</h2>"
+            //         + "<p>Your OTP is: <strong>" + otp + "</strong></p>"
+            //         + "<p>Please use this code within 10 minutes. Do not share it with anyone.</p>"
+            //         + "<p>Note: OTP will expire in 10 minutes.</p></body></html>";
+            // helper.setText(htmlBody, true);
+            // mailSender.send(message);
             return true;
-        } catch (MessagingException e) {
+        } catch (Exception e) {
             System.out.println(e);
             return false;
         }
