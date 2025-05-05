f3adf6a82b17e0c0ba638637ae4e93f23036a303
Azan Baloch <balochazan36447@gmail.com>
2025-05-06 00:25:04 +0500

    updated default homepage json

--- src/main/java/com/buildazan/service/VerificationService.java
+++ src/main/java/com/buildazan/service/VerificationService.java
@@ -3,7 +3,6 @@ package com.buildazan.service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
